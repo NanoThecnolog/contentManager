a94f0264b19648eb5bdb5c028b34c76f5684eb16
NanoThecnolog <contato@ericssongomes.com>
2025-03-31 20:05:17 -0300

    Configurando cors

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,14 @@ const port = process.env.PORT
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://flixnext.com.br', 'https://flixnext.netlify.app'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'key'],
+    credentials: true,
+  });
+
   await app.listen(port ?? 5647);
   console.log(`Servidor de gerenciamento de conteúdo online! Porta ${port}`)
 }
