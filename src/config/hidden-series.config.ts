3fd713e72a6bf993423cf45c83ba4e1a885891ab
NanoThecnolog <contato@ericssongomes.com>
2026-08-13 00:17:59 -0300

    config: configura series para remover temporariamente do catálogo

--- src/config/hidden-series.config.ts
+++ src/config/hidden-series.config.ts
@@ -5,5 +5,13 @@
  * os registros armazenados no MongoDB.
  */
 export const hiddenSeriesTmdbIds = new Set<number>([
-  194766, 109939, 74577, 126308, 118906, 4604,
+  194766, //verão q mudou a vida dela
+  74577, //end of the fucking world
+  126308, // xogum
+  118906, // universos paralelos
+  4604, // smallville
+  109939, //nossa bandeira é a morte
+  153784, //ta tudo certo
+  89901, //dickinson
+  100757, //outerbanks
 ]);
